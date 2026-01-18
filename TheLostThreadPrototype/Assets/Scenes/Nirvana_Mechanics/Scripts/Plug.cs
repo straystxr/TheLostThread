1635efeee1166aa0224a6250aea202a8b315b676
Nirvana <nirvana.vella.g53246@mcast.edu.mt>
2026-01-18 16:36:28 +0100

    plug script added not done

---
+++ TheLostThreadPrototype/Assets/Scenes/Nirvana_Mechanics/Scripts/Plug.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public enum PlugOrder
+{
+    first,
+    second,
+    third,
+    fourth
+}
+public class Plug : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
