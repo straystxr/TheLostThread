357d7d60d83d607a3bc85956445c3e3e06042d65
RGBAgni <aarchipovaite@gmail.com>
2026-01-27 00:09:53 +0100

    feat: menu, scripting

---
+++ TheLostThreadPrototype/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,23 @@
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit(); 
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        
+    }
+    
+}
